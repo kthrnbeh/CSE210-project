e412d8fc2bfd0d7e8348935afdcc1e9316b0da47
kthrnbeh <kthrnbeh@byui.edu>
2025-01-07 09:45:32 -0700

    First Part

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?" ); // Get first name, keep on the same line
+        string questionone=Console.ReadLine(); // 
+        Console.Write("What is your last name?" ); // Get last name, keep on same line 
+        string questiontwo = Console.ReadLine();
+        Console.WriteLine($"Your name is {questiontwo}, {questionone} {questiontwo}"); // Use brackets to add in info
     }
 }
