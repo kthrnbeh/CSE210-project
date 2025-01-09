3a0b713f60d917cf97095410657cdd4708e45bc6
kthrnbeh <kthrnbeh@byui.edu>
2025-01-09 11:21:17 -0700

    Stretch challeng

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Data.Common;
+using System.Diagnostics.Metrics;
+using System.Runtime;
 
 class Program
 {
@@ -19,34 +21,59 @@ class Program
         //Console.Write("Please input a magic number:");
         //int magic = int.Parse(Console.ReadLine());
         //Changed it to random
+        
+        // to keep guessing
+        string keepGuess= "yes";
+        //Stretch count the amount of guesses 
+        int guessAmount=0;
+     //Originally the loop messed up from the yes
         Random randomGenerator = new Random();
         int magic = randomGenerator.Next(1,100);
-        //ask user to input gress for the number
-        Console.Write("Please give me a guess for the magic number?");
-        int number= int.Parse(Console.ReadLine());
-        //When  the program begins it doesn't tell you higher
-        //or lower on the first one. I wonder how to do that. 
-        //Check the guess with if statement
-        while (magic != number)
-        {
             //ask user to input gress for the number
+        int number = 0;
+
+    //if say yes they can keep playing 
+        while (keepGuess.ToLower() == "yes") // got help with the lower part in fixing my code.
+             
             Console.Write("Please give me a guess for the magic number?");
             number= int.Parse(Console.ReadLine());
-            if (magic==number)
-            {
-                Console.WriteLine("You did it!");
+            guessAmount ++;
+            //When  the program begins it doesn't tell you higher
+            //or lower on the first one. I wonder how to do that. 
+             //Check the guess with if statement
+             
+             // Apparently needed to move these all to the While look to make the yes work.
+        {
+            while (magic != number)
+           {
+                //ask user to input gress for the number
+                Console.Write("Please give me a guess for the magic number?");
+                number= int.Parse(Console.ReadLine());
+                guessAmount += 1;
+                //if guess is right.
+                if (magic==number)
+                {
+                    Console.WriteLine("You did it!");
 
-            }
-            else if ( magic > number)
-            {
-                Console.WriteLine("Higher!");
+                }
+                //if guess needs to be higher.
+                else if ( magic > number)
+                {
+                    Console.WriteLine("Higher!");
 
-            }
-            else
-            {
-                Console.WriteLine("Lower!");
+                }
+                // If the guess is to low.
+                else
+                {
+                    Console.WriteLine("Lower!");
 
-            }
+                }
+
+             Console.WriteLine($"The amount of guesses it took was {guessAmount}.");
+             Console.WriteLine($"Would you like to keep playing reply yes/no?{keepGuess}");
+             keepGuess = Console.ReadLine().ToLower();
         }
+        Console.WriteLine("Thanks for playing. Goodbye.");
+             }
    }
 }
